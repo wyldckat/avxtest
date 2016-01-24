d8c592c831ff37d46bf0ea2022bfcbb1eeab3270
wyldckat <wyldckat@gmail.com>
2016-01-24 09:36:17 +0000

    generateTimingsAndWikiPage.py: Added hack and description of hack for Python 2.6

--- generateTimingsAndWikiPage.py
+++ generateTimingsAndWikiPage.py
@@ -25,7 +25,61 @@ import multiprocessing
 
 '''Functions'''
 
-'''Shoutout to http://calebmadrigal.com/standard-deviation-in-python/ for the concept average and stddev code'''
+'''
+Original source code for this hack for Python 2.6: http://pydoc.net/Python/pep8radius/0.9.0/
+License: MIT License as well
+Author: Andy Hayden
+'''
+"""Note: We also monkey-patch subprocess for python 2.6 to
+give feature parity with later versions.
+"""
+try:
+    from subprocess import STDOUT, check_output, CalledProcessError
+except ImportError:  # pragma: no cover
+    # python 2.6 doesn't include check_output
+    # monkey patch it in!
+    import subprocess
+    STDOUT = subprocess.STDOUT
+
+    def check_output(*popenargs, **kwargs):
+        if 'stdout' in kwargs:  # pragma: no cover
+            raise ValueError('stdout argument not allowed, '
+                             'it will be overridden.')
+        process = subprocess.Popen(stdout=subprocess.PIPE,
+                                   *popenargs, **kwargs)
+        output, _ = process.communicate()
+        retcode = process.poll()
+        if retcode:
+            cmd = kwargs.get("args")
+            if cmd is None:
+                cmd = popenargs[0]
+            raise subprocess.CalledProcessError(retcode, cmd,
+                                                output=output)
+        return output
+    subprocess.check_output = check_output
+
+    # overwrite CalledProcessError due to `output`
+    # keyword not being available (in 2.6)
+    class CalledProcessError(Exception):
+
+        def __init__(self, returncode, cmd, output=None):
+            self.returncode = returncode
+            self.cmd = cmd
+            self.output = output
+
+        def __str__(self):
+            return "Command '%s' returned non-zero exit status %d" % (
+                self.cmd, self.returncode)
+    subprocess.CalledProcessError = CalledProcessError
+    
+'''End of hack for Python 2.6.
+'''
+
+
+'''
+Shoutout to http://calebmadrigal.com/standard-deviation-in-python/ for the
+concept code of average and stddev
+'''
 
 def average(arr, start=0, end=-1):
 
